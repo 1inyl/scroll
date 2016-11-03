aa644caa3600708f4cb6cee94158472bfecb9550
1in <546798190@qq.com>
2016-11-04 02:51:51 +0800

    setInterval

--- scroll/index.js
+++ scroll/index.js
@@ -10,28 +10,23 @@ window.onload = function() {
 
 
     left.onclick = function() {
-        // for (var i = 0; i < imgs.length; i++) {
               var active
           if (active = visible) {
               var index = active.getAttribute('alt') - 1  //  0,1,2,3,4,
             if (index > 0) {
-              console.log(index);
               imgs[index].setAttribute('id', '')
               imgs[index - 1].setAttribute('id', 'visible')
             } else {
               imgs[index].setAttribute('id', '')
-              imgs[4].setAttribute('id', 'visible')
+              imgs[imgs.length - 1].setAttribute('id', 'visible')
             }
-          }
-        // }
     }
+  }
 
     right.onclick = function() {
-        // for (var i = 0; i < imgs.length; i++) {
           if (active = visible) {
               var index = active.getAttribute('alt') - 1  //  0,1,2,3,4,
-            if (index < 4) {
-              console.log(index);
+            if (index < imgs.length - 1) {
               imgs[index].setAttribute('id', '')
               imgs[index + 1].setAttribute('id', 'visible')
             } else {
@@ -39,45 +34,48 @@ window.onload = function() {
               imgs[0].setAttribute('id', 'visible')
             }
           }
-        // }
     }
 
 
-    // turnList.onclick = function(e) {
-    //     var evt = e || window.event
-    //     var target = evt.target || evt.srcElement
-    //     Array.prototype.forEach.call(btnTurn, function(li, index) {
-    //         if (li === target) {
-    //             for (var i = 0; i < btnTurn.length; i++) {
-    //                 img[i].setAttribute('id', '')
-    //                 img[index].setAttribute('id', 'visible')
-    //             }
-    //         }
-    //     })
-    // }
-    //
-    // var scroll = function () { // ture false
-    //   var active = document.getElementById('visible')
-    //   var num = active.alt - 1
-    //
-    //   if (num > img.length - 2) {
-    //     num = 0;
-    //   }
-    //   img[num].setAttribute('id', '')
-    //   img[num + 1].setAttribute('id', 'visible')
-    // }
-    //
-    //
-    // var speed = 1000
-    //
-    // var scroll = setInterval(scroll, speed)
-    // main.onmouseover = function() {
-    //     clearInterval(scroll)
-    // }
-    //
-    // main.onmouseout = function() {
-    //     scroll = setInterval(scroll, speed)
-    // }
-    //
+    turnList.onclick = function(e) {
+        var evt = e || window.event
+        var target = evt.target || evt.srcElement
+        Array.prototype.forEach.call(btnTurn, function(li, index) {
+            if (li === target) {
+                for (var i = 0; i < btnTurn.length; i++) {
+                    imgs[i].setAttribute('id', '')
+                    imgs[index].setAttribute('id', 'visible')
+                }
+            }
+        })
+    }
+
+
+    var scroll = function () { // ture false
+      var active = document.getElementById('visible')
+      var index = active.alt - 1
+      if (index > imgs.length - 2) {
+        index = 0;
+        imgs[index].setAttribute('id', 'visible')
+        imgs[imgs.length - 1].setAttribute('id', '')
+      }
+        else {
+          imgs[index].setAttribute('id', '')
+          imgs[index + 1].setAttribute('id', 'visible')
+        }
+
+    }
+
+
+    var speed = 1500
+
+    var scroll = setInterval(scroll, speed)
+    main.onmouseover = function() {
+        clearInterval(scroll)
+    }
+
+    main.onmouseout = function() {
+        scroll = setInterval(scroll, speed)
+    }
 
 }
