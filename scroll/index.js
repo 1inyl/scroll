0984cfae50e1ff992aa80416fa54f21348dc4bf6
1in <546798190@qq.com>
2016-11-04 01:53:02 +0800

    LR-scroll

--- scroll/index.js
+++ scroll/index.js
@@ -1,11 +1,47 @@
 window.onload = function() {
     var main = document.querySelector('.container')
     var content = document.querySelector('.content')
-    var img = document.querySelectorAll('.content img')
+    var imgs = document.querySelectorAll('.content img')
     var btnTurn = document.querySelectorAll('.turn')
     var turnList = document.querySelector('.scroll-turn')
     var left = document.getElementById('left')
     var right = document.getElementById('right')
+
+
+
+    left.onclick = function() {
+        // for (var i = 0; i < imgs.length; i++) {
+              var active
+          if (active = visible) {
+              var index = active.getAttribute('alt') - 1  //  0,1,2,3,4,
+            if (index > 0) {
+              console.log(index);
+              imgs[index].setAttribute('id', '')
+              imgs[index - 1].setAttribute('id', 'visible')
+            } else {
+              imgs[index].setAttribute('id', '')
+              imgs[4].setAttribute('id', 'visible')
+            }
+          }
+        // }
+    }
+
+    right.onclick = function() {
+        // for (var i = 0; i < imgs.length; i++) {
+          if (active = visible) {
+              var index = active.getAttribute('alt') - 1  //  0,1,2,3,4,
+            if (index < 4) {
+              console.log(index);
+              imgs[index].setAttribute('id', '')
+              imgs[index + 1].setAttribute('id', 'visible')
+            } else {
+              imgs[index].setAttribute('id', '')
+              imgs[0].setAttribute('id', 'visible')
+            }
+          }
+        // }
+    }
+
 
     // turnList.onclick = function(e) {
     //     var evt = e || window.event
